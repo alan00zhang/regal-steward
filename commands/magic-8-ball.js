c86c7a9fa1ab4a965841b88605ed92fb047f027f
alan00zhang <62392131+alan00zhang@users.noreply.github.com>
2022-09-24 01:36:43 -0400

    Added Magic 8 Ball feature

---
+++ commands/magic-8-ball.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('discord.js');
+const negativeResponses = [
+	"Don’t count on it.", "My reply is no.", "My sources say no.",
+	"Outlook not so good.",	"Very doubtful."
+];
+
+const positiveResponses = [
+	"It is certain.", "It is decidedly so.", "Without a doubt.",	"Yes definitely.",	
+	"You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", 
+	"Yes.", "Signs point to yes."
+];
+
+const unsureResponses = [
+	"Better not tell you now.",	"Cannot predict now.",	"Reply hazy, try again.", 
+	"Concentrate and ask again.",	"Ask again later."
+];
+
+const allResponses = [
+	...negativeResponses, ...positiveResponses, ...unsureResponses
+]
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('magic-8-ball')
+		.setDescription('Ask the all-knowing and receive truth unbound'),
+	async execute(interaction) {
+    let randomIndex = Math.floor(Math.random() * allResponses.length);
+		await interaction.reply(allResponses[randomIndex]);
+	},
+};
