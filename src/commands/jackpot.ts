6f01c08489be39cbbb9580f9d366d9ee37c92122
Alan Zhang <alan00zhang@gmail.com>
2024-05-26 21:07:47 -0400

    fixed jackpot rounding and cost calculations

--- src/commands/jackpot.ts
+++ src/commands/jackpot.ts
@@ -2,9 +2,6 @@ import { SlashCommandBuilder, ChatInputCommandInteraction, GuildMember } from 'd
 import { Utils } from '../utils.js';
 import { System } from '../systems/systems.js';
 import { AppCommand } from '../types.js';
-import { BankAccount } from '../systems/bank.js';
-
-// The price of betting on a jackpot roll once
 
 export const CommandJackpot: AppCommand = {
 	async execute(interaction: ChatInputCommandInteraction, system: System) {
@@ -13,7 +10,7 @@ export const CommandJackpot: AppCommand = {
 		const banker = await system.bank.getUserAccount("bank");
 		let betCost = Math.round(banker.bankBalance) / 100;
 		// cap the betCost at 100,000.00
-		if (betCost > 100000.00) betCost = 100000.00;
+		if (betCost > 100_000.00) betCost = 100_000.00;
 		if (interaction.options.getSubcommand() === "check") {
 			let response = `The jackpot is currently valued at ${Utils.Units.bankPrefix} ${banker.bankBalanceString}.
 			The cost of a jackpot bet is ${Utils.Units.bankPrefix} ${Utils.formatCurrency(betCost)}.`;
@@ -29,20 +26,20 @@ export const CommandJackpot: AppCommand = {
 				});
 				return;
 			}
-			await bankAccount.subtractBank(betCost * 100);
+			await bankAccount.subtractBank(betCost);
 			let roll = Math.floor(Math.random() * 100) + 1;
 			let response = `<@${member.id}> is taking a chance at the jackpot!\n\nThey rolled a ${roll === 100 ? "⭐100⭐!!!" : roll}\n`;
 			if (roll === 100) {
 				let jackpot = banker.bankBalance;
-				await bankAccount.addBank(banker.userData.bank_amount);
-				await bankAccount.addCasinoWinnings(banker.userData.bank_amount);
-				await banker.subtractBank(banker.userData.bank_amount);
+				await bankAccount.addBank(banker.bankBalance);
+				await bankAccount.addCasinoWinnings(banker.bankBalance);
+				await banker.subtractBank(banker.bankBalance);
 				response += `**Congratulations! <@${member.id}> just won the BIG POT of ${Utils.Units.bankPrefix} ${jackpot}!!!**`
 			} else if (roll > 90) {
-				await bankAccount.addCasinoLosses(betCost * 100);
+				await bankAccount.addCasinoLosses(betCost);
 				response += `🇱 Soo close... try again?`
 			} else {
-				await bankAccount.addCasinoLosses(betCost * 100);
+				await bankAccount.addCasinoLosses(betCost);
 				response += `🇱 That's tough... better luck next time.`
 			}
 			await interaction.reply({
