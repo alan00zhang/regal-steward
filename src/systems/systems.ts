a799cdd11ba4f012a2cde6f2b415b85abed33998
Alan Zhang <alan00zhang@gmail.com>
2024-05-28 00:49:13 -0400

    refactored and added database.ts

--- src/systems/systems.ts
+++ src/systems/systems.ts
@@ -5,7 +5,7 @@ const __dirname = dirname(__filename);
 
 import * as dotenv from "dotenv";
 dotenv.config({path: __dirname + '/.env'});
-import { ButtonInteraction, ChatInputCommandInteraction, Client, Interaction, InteractionType, MessageComponentInteraction } from 'discord.js';
+import { ChatInputCommandInteraction, Client, Interaction, InteractionType, MessageComponentInteraction } from 'discord.js';
 import { SalaryService } from '../services/salary-service.js';
 import { MemeService } from '../services/meme-service.js';
 import { Bank } from './bank.js';
@@ -16,6 +16,7 @@ import { CommandJackpot } from '../commands/jackpot.js';
 import { CommandCasinoLeaderboards } from '../commands/casino-leaderboards.js';
 import { CommandTip } from '../commands/tip.js';
 import { CommandCrownAndAnchor } from '../commands/crown-and-anchor.js';
+import { UniqueSystemDatabase, BasicSystemDatabase } from './database.js';
 
 export type UniqueItem<T>= {
   id: string,
@@ -138,70 +139,5 @@ export class System {
       this.client.removeListener("interactionCreate", interactionFn);
     }, eventOptions.duration);
     return interactionFn;
-  }
-}
-
-// Database class with basic CRUD operations
-export abstract class AbstractSystemDatabase<T> {
-  public name: string;
-  protected abstract _db: any;
-
-  constructor(name: string) {
-    this.name = name;
-  }
-
-  abstract get(value: T): T;
-  abstract store(item: T): void;
-  abstract delete(item: T): void;
-  abstract deleteByIndex(index: number): void;
-}
-
-export class BasicSystemDatabase<T> extends AbstractSystemDatabase<T> {
-  protected _db: T[];
-
-  constructor(name: string) {
-    super(name);
-    this._db = [];
-  }
-
-  public get(value: T): T {
-    return this._db.find(item => item === value);
-  }
-
-  public store(item: T): void {
-    this._db.push(item);
-  }
-
-  public delete(item: T): void {
-    let index = this._db.indexOf(item);
-    if (index !== -1) this._db.splice(index, 1);
-    else console.warn(`Could not find item ${item} in ${this.name} database!`);
-  }
-
-  public deleteByIndex(index: number): void {
-    try {
-      this._db.splice(index, 1);
-    } catch(error) {
-      console.warn(`Could not delete index ${index} in ${this.name} database!`);
-    }
-  }
-}
-
-export class UniqueSystemDatabase<T> extends BasicSystemDatabase<T> {
-  constructor(name: string) {
-    super(name);
-  }
-
-  has(item: T) {
-    return this.get(item) !== undefined;
-  }
-
-  store(item: T, overwrite?: boolean) {
-    let existingItem = this.get(item);
-    if (!existingItem) {
-      super.store(item);
-    } else {
-      if (overwrite) existingItem = item;
-    }
   }
 }
