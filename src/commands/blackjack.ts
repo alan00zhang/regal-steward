6279251ffe3b74b9a512799bdadbe3ffe56ac2df
Alan Zhang <alan00zhang@gmail.com>
2024-06-06 23:16:05 -0400

    finished blackjack implementation

--- src/commands/blackjack.ts
+++ src/commands/blackjack.ts
@@ -2,13 +2,15 @@ import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChatInputCommandInteracti
 import { AppCommand } from "../types.js";
 import { System } from "../systems/systems.js";
 import { Utils } from "../utils.js";
-import { BlackjackDealer, BlackjackGame } from '../components/casino/blackjack/blackjack-game.js';
+import { BlackjackDealerGame, BlackjackGame } from '../components/casino/blackjack/blackjack-game.js';
+import { Subscription } from "rxjs";
 
 export const CommandBlackjack: AppCommand = {
   async execute(interaction: ChatInputCommandInteraction, system: System) {
     const bet = Math.round(interaction.options.getNumber("bet") * 1e2) / 1e2; // round to .00 decimals
     let member = <GuildMember>interaction.member;
     let userAccount = await system.bank.getUserAccount(member.id);
+		const banker = await system.bank.getUserAccount("bank");
     const Casino = system.Casino;
     if (userAccount.bankBalance < bet || bet < 0) {
       await interaction.reply({
@@ -34,12 +36,14 @@ export const CommandBlackjack: AppCommand = {
       .setLabel("Join")
       .setStyle(ButtonStyle.Success);
 
+    let lobbyMesssage = `Let's play a game of blackjack! Click below to join the table!`;
     let lobby = await interaction.reply({
-      content: `Let's play a game of blackjack! Click below to join the table!`,
+      content: lobbyMesssage,
       components: [new ActionRowBuilder<ButtonBuilder>().addComponents(joinButton)]
     });
+    let subscriptions = new Subscription();
     try {
-      const lobbyCollector = lobby.createMessageComponentCollector({ componentType: ComponentType.Button, time: 15_000 });
+      const lobbyCollector = lobby.createMessageComponentCollector({ componentType: ComponentType.Button });
       let players: Record<string, InteractionResponse> = {}
       lobbyCollector.on("collect", async joinInteraction => {
         let player = <GuildMember>joinInteraction.member;
@@ -51,39 +55,62 @@ export const CommandBlackjack: AppCommand = {
           content: `Welcome to the table. \nThe game will start soon...`,
           ephemeral: true
         })
-        players[player.id] = game;
+        players[player.id] = game;  
       });
-      lobbyCollector.on("ignore", () => {
+      for (let i = 10; i > 0; --i) {
         lobby.edit({
-          content: `No gamblers?`
-        });
-        throw new Error("No one joined this blackjack table");
-      })
-      await Utils.delay(5_000);
+          content: lobbyMesssage + ` (${i})`
+        })
+        await Utils.delay(1000);
+      }
+      lobbyCollector.stop();
+      if (Object.keys(players).length === 0) throw new Error("No players!")
 
       dealer.shuffle();
-      let dealerGame = new BlackjackDealer(Casino, dealer, lobby);
+      let dealerGame = new BlackjackDealerGame(Casino, dealer, lobby, command);
       dealerGame.dealerStart();
       let games: BlackjackGame[] = [];
       for (let player in players) {
-        games.push(new BlackjackGame(Casino, dealer, players[player]));
+        games.push(new BlackjackGame(Casino, dealer, players[player], bet, userAccount));
       }
+      // await dealer blackjack
       for (let game of games) {
-        game.dealIn();
-        game.finished.subscribe(() => {
+        game.dealIn(dealerGame);
+        subscriptions.add(game.finished$.subscribe(() => {
           // if every game is finished
-          if (games.every(instance => instance.finished.value)) {
-            dealerGame.dealerEnd();
+          if (games.every(instance => instance.finished$.value)) {
+            dealerGame.reveal();
           }
-        });
+        }));
+        subscriptions.add(game.score$.subscribe(() => {
+          if (games.every(instance => instance.score$.value !== undefined)) {
+            let content = `All hands paid out, thanks for playing blackjack! This dealer is closing down.`;
+            for (let game of games) {
+              let outcome = game.score$.value < 0 ? "lost" : "won";
+              let score = Math.abs(game.score$.value);
+              if (game.score$.value < 0) {
+                game.userAccount.subtractBank(score);
+                game.userAccount.addCasinoLosses(score);
+                banker.addBank(score);
+              } else {
+                game.userAccount.addBank(score);
+                game.userAccount.addCasinoWinnings(score);
+              }
+              content += `\n<@${game.interaction.interaction.user.id}> played and ${outcome} ${Utils.Units.bankPrefix} ${Utils.formatCurrency(score)}`;
+            }
+            interaction.channel.send({
+              content: content
+            })
+            dealerGame.endGame();
+            subscriptions.unsubscribe();
+          }
+        }));
       }
     } catch (error) {
-      interaction.channel.send({
-        content: `No one joined game, dealer is closing this table down...`
-      })
-    } finally {
       command.close();
-      dealer.closeTable();
+      interaction.channel.send({
+        content: `No one joined the game, dealer is closing this table down...`
+      })
     }
   }
 }
--- src/components/casino/blackjack/blackjack-game.ts
+++ src/components/casino/blackjack/blackjack-game.ts
@@ -1,23 +1,31 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType, InteractionResponse, Message } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, ComponentType, InteractionCollector, InteractionResponse, Message } from "discord.js";
 import { CasinoService } from "../../../services/casino-service.js";
 import { Utils } from "../../../utils.js";
-import { CardNumber } from "../../../types.js";
-import { BehaviorSubject } from "rxjs";
+import { CardNumber, SingletonCommand } from "../../../types.js";
+import { BehaviorSubject, Subject, Subscription } from "rxjs";
 import { BLACKJACK_VALUES } from "../../../constants.js";
 import { Dealer } from "../dealer.js";
 import { Hand } from "../hand.js";
+import { BankAccount } from "../../../systems/bank.js";
 
 export class BlackjackGame {
   interaction: InteractionResponse;
   Casino: CasinoService;
   dealer: Dealer;
-  dealerMsg?: Message;
   hand: Hand;
-  finished = new BehaviorSubject<boolean>(false);
-  constructor(casino: CasinoService, dealer: Dealer, interaction: InteractionResponse) {
+  message: Message;
+  finished$ = new BehaviorSubject<boolean | string>(false);
+  score$ = new BehaviorSubject<number>(undefined);
+  collector: InteractionCollector<ButtonInteraction>;
+  bet: number;
+  userAccount: BankAccount;
+  private subscriptions = new Subscription();
+  constructor(casino: CasinoService, dealer: Dealer, interaction: InteractionResponse, bet?: number, userAccount?: BankAccount) {
     this.interaction = interaction;
     this.Casino = casino;
     this.dealer = dealer;
+    this.bet = bet;
+    this.userAccount = userAccount;
   }
   get total() {
     let total = 0;
@@ -34,27 +42,79 @@ export class BlackjackGame {
     }
     return total;
   }
-  async dealIn() {
+  async dealIn(dealerGame: BlackjackDealerGame) {
     this.hand = this.dealer.getHand();
-    let message: Message;
     for (let i = 0; i < 2; ++i) {
       await Utils.delay(1000);
-      message = await this.hit() as Message;
+      await this.hit();
     }
-    let collector = message.createMessageComponentCollector({ componentType: ComponentType.Button, time: Utils.Time.MINUTE15});
-    collector.on("collect", async interaction => {
+    await Utils.delay(1000);
+    this.startCollector();
+    let dealerHasBlackjack = await this.subscribeToDealer(dealerGame);
+    if (dealerHasBlackjack !== "blackjack") {
+      if (this.total === 21) {
+        this.blackjack();
+        return;
+      }
+      this.displayButtons();
+    }
+  }
+  private async startCollector() {
+    this.collector = this.message.createMessageComponentCollector({ componentType: ComponentType.Button, time: Utils.Time.MINUTE5});
+    this.collector.on("collect", async interaction => {
       if (interaction.customId === ("hit")) {
-        if (!await this.hit()) {
+        let total = await this.hit();
+        if (total > 21) {
           this.bust();
-          collector.stop();
+        } else if (total === 21) {
+          this.stand();
         }
       } else if (interaction.customId === "stand") {
         this.stand();
-        collector.stop();
       }
     })
+    this.collector.on("ignore", () => {
+      this.finished$.next(true);
+      this.score$.next(-this.bet)
+    });
   }
   async hit() {
+    this.dealer.deal(this.hand);
+    let total = this.total;
+    let img = await this.Casino.displayCards(this.hand.cards);
+    this.message = await this.interaction.edit({
+      content: `Your hand (total: ${total})`,
+      files: [img]
+    });
+    return total;
+  }
+  async stand() {
+    this.finished$.next(true);
+    await this.interaction.edit({
+      content: `Waiting for all players to finish. Your total: ${this.total}`,
+      components: []
+    });
+    this.collector.stop();
+  }
+  async bust() {
+    this.finished$.next(true);
+    await this.interaction.edit({
+      content: `BUST! You lost this hand.`,
+      components: []
+    });
+    this.collector.stop();
+    this.endGame(-this.bet);
+  }
+  async blackjack() {
+    this.finished$.next(true);
+    await this.interaction.edit({
+      content: `BLACKJACK BABY! You win!`,
+      components: []
+    });
+    this.collector.stop();
+    this.endGame(this.bet * 1.5);
+  }
+  async displayButtons() {
     let hitButton = new ButtonBuilder()
     .setCustomId(`hit`)
     .setLabel("Hit")
@@ -65,41 +125,74 @@ export class BlackjackGame {
       .setStyle(ButtonStyle.Secondary)
     let row = new ActionRowBuilder<ButtonBuilder>()
       .addComponents(hitButton, standButton)
-    
-    this.dealer.deal(this.hand);
-    let total = this.total;
-    let img = await this.Casino.displayCards(this.hand.cards);
-    let message = await this.interaction.edit({
-      content: `Your hand (total: ${total})`,
-      components: [row],
-      files: [img]
-    });
-    return total < 21 ? message : false;
+    this.message = await this.interaction.edit({
+      components: [row]
+    })
   }
-  async stand() {
-    this.finished.next(true);
-    await this.interaction.edit({
-      content: `Waiting for all players to finish`,
-      components: []
-    });
+  async subscribeToDealer(game: BlackjackDealerGame) {
+    this.subscriptions.add(game.finished$.subscribe(value => {
+      if (value) {
+        if (value === "bust") {
+          this.payout();
+        } else if (value === "blackjack") {
+          this.lose(true);
+        } else {
+          if (game.total > this.total) {
+            this.lose();
+          } else if (game.total === this.total) {
+            this.push();
+          } else {
+            this.payout();
+          }
+        }
+      }
+    }));
+    return game.finished$.value;
   }
-  async bust() {
-    this.finished.next(true);
-    await this.interaction.edit({
-      content: `BUST! You lost this hand.`,
-      components: []
+  private async push() {
+    this.message = await this.interaction.edit({
+      content: `Huh, it's a tie.`
     });
+    this.endGame(0);
   }
-  async blackjack() {
-    this.finished.next(true);
-    await this.interaction.edit({
-      content: `BLACKJACK BABY! You win!`,
-      components: []
+  private async payout() {
+    this.message = await this.interaction.edit({
+      content: `You win!`
     });
+    this.endGame(this.bet);
+  }
+  private async lose(blackjack: boolean = false) {
+    if (blackjack) {
+      if (this.total === 21) {
+        await this.interaction.edit({
+          content: `Dealer blackjack... but you also had blackjack. Saved.`
+        });
+        this.endGame(0);
+        return;
+      }
+      await this.interaction.edit({
+        content: `Dealer blackjack... never even stood a chance.`
+      });
+      this.endGame(-this.bet)
+      return
+    }
+    await this.interaction.edit({
+      content: `Dealer has a higher total than you. You lose! You lose!`
+    });
+    this.endGame(-this.bet);
+  }
+  async endGame(score: number) {
+    this.score$.next(score);
+    this.subscriptions.unsubscribe();
   }
 }
 
-export class BlackjackDealer extends BlackjackGame {
+export class BlackjackDealerGame extends BlackjackGame {
+  command: SingletonCommand;
+  constructor(Casino: CasinoService, dealer: Dealer, interaction: InteractionResponse, command: SingletonCommand) {
+    super(Casino, dealer, interaction);
+    this.command = command;
+  }
   get total() {
     let total = 0;
     for (let card of this.dealer.cards) {
@@ -115,32 +208,94 @@ export class BlackjackDealer extends BlackjackGame {
     }
     return total;
   }
+  get realTotal() {
+    let total = 0;
+    for (let card of this.dealer.cards) {
+      let value: number;
+      if (card.number !== CardNumber.Ace) {
+        value = BLACKJACK_VALUES[card.number];
+      } else {
+        value = total < 11 ? 11 : 1;
+      }
+      total += value;
+    }
+    return total;
+  }
   async dealerStart() {
     let content = `The game has started! Welcome to the torture chamber.`
-    let dealerMsg = await this.interaction.interaction.channel.send({
+    this.message = await this.interaction.interaction.channel.send({
       content: content
     });
     for (let i = 0; i < 2; ++i) {
       await Utils.delay(1000);
       this.dealer.draw(this.dealer.deck, i === 0);
       let img = await this.Casino.displayCards(this.dealer.cards);
-      dealerMsg = await dealerMsg.edit({
+      this.message = await this.message.edit({
         content: content += ".",
         files: [img]
       });
     }
-    await Utils.delay(1500);
-    dealerMsg = await dealerMsg.edit({
-      content: `Dealer's hand (total: ${this.total})`
-    });
-    this.dealerMsg = dealerMsg;
+    await Utils.delay(1000);
+    if (this.realTotal === 21) {
+      this.blackjack();
+    } else {
+      this.message = await this.message.edit({
+        content: `Dealer's hand (total: ${this.total})`
+      });
+    }
   }
-  async dealerEnd() {
+  async reveal() {
+    await Utils.delay(1000);
     this.dealer.reveal();
     let img = await this.Casino.displayCards(this.dealer.cards);
-    await this.dealerMsg.edit({
-      content: `Dealer's hand (total: ${this.total})`,
+    this.message = await this.message.edit({
+      content: `All games finished. Dealer reveals hand. (total: ${this.total})`,
       files: [img]
     });
+    // Dealer "AI" starts here
+    await Utils.delay(2000);
+    while (this.total <= 16) {
+      await this.hit();
+      await Utils.delay(1000);
+    }
+    if (this.total > 21) {
+      this.bust();
+    } else {
+      this.stand();
+    }
+  }
+  async hit(hide?: boolean) {
+    this.dealer.draw(this.dealer.deck, hide);
+    let img = await this.Casino.displayCards(this.dealer.cards);
+    this.message = await this.message.edit({
+      content: `Dealer hits. (total: ${this.total})`,
+      files: [img]
+    });
+    return this.total;
+  }
+  async stand() {
+    this.finished$.next(true);
+    this.message = await this.message.edit({
+      content: `Dealer stands. (total: ${this.total})`
+    })
+  }
+  async bust() {
+    this.finished$.next("bust");
+    this.message = await this.message.edit({
+      content: `Dealer busts! Everyone left wins!`
+    });
+  }
+  async blackjack() {
+    this.finished$.next("blackjack");
+    this.dealer.reveal();
+    let img = await this.Casino.displayCards(this.dealer.cards);
+    this.message = await this.message.edit({
+      content: `Dealer blackjack!`,
+      files: [img]
+    });
+  }
+  async endGame() {
+    this.dealer.closeTable();
+    this.command.close();
   }
 }
