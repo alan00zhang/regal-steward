029de9fbc12b1acb680b20a8708a20b164f51ba5
Alan Zhang <alan00zhang@gmail.com>
2024-06-26 13:34:05 -0400

    minor refactor

--- src/services/audio-service.ts
+++ src/services/audio-service.ts
@@ -1,13 +1,23 @@
 import { SystemService } from "./service.js";
-import { AudioPlayer, StreamType, VoiceConnectionStatus, createAudioPlayer, createAudioResource, entersState, getVoiceConnection, joinVoiceChannel } from "@discordjs/voice";
+import {
+  AudioPlayer,
+  StreamType,
+  VoiceConnectionStatus,
+  createAudioPlayer,
+  createAudioResource,
+  entersState,
+  getVoiceConnection,
+  joinVoiceChannel,
+} from "@discordjs/voice";
 import YTSR from "@distube/ytsr";
 import YTDL from "ytdl-core";
 import { Message } from "discord.js";
 import { UniqueKeySystemDatabase } from "../systems/database.js";
 import { AudioDownloader } from "../components/audio/audio-downloader.js";
+import { GuildAudioPlayer } from "../components/audio/guild-audio-player.js";
 
 export class AudioService extends SystemService {
-  players: UniqueKeySystemDatabase<"guildId">;
+  players: UniqueKeySystemDatabase<GuildAudioPlayer>;
   service(): void {
     this.players = new UniqueKeySystemDatabase("players", "guildId");
     this.listenToPlay();
@@ -16,7 +26,7 @@ export class AudioService extends SystemService {
     this.listenToLeave();
   }
   listenToPlay() {
-    this.system.client.on("messageCreate", message => {
+    this.system.client.on("messageCreate", (message) => {
       if (message.content.startsWith("!play") && message.member.voice.channel?.id) {
         this.join(message);
         this.play(message);
@@ -24,7 +34,7 @@ export class AudioService extends SystemService {
     });
   }
   listenToPause() {
-    this.system.client.on("messageCreate", message => {
+    this.system.client.on("messageCreate", (message) => {
       if (message.content.startsWith("!pause")) {
         const player = this.players.getByID(message.guildId).player;
         player?.pause();
@@ -32,7 +42,7 @@ export class AudioService extends SystemService {
     });
   }
   listenToStop() {
-    this.system.client.on("messageCreate", message => {
+    this.system.client.on("messageCreate", (message) => {
       if (message.content.startsWith("!stop")) {
         const player = this.players.getByID(message.guildId).player;
         player?.stop();
@@ -40,7 +50,7 @@ export class AudioService extends SystemService {
     });
   }
   listenToLeave() {
-    this.system.client.on("messageCreate", message => {
+    this.system.client.on("messageCreate", (message) => {
       if (message.content.startsWith("!leave")) {
         let connection = getVoiceConnection(message.guildId);
         connection?.disconnect();
@@ -53,13 +63,13 @@ export class AudioService extends SystemService {
       guildId: message.guildId,
       channelId: message.member.voice.channel.id,
       adapterCreator: message.guild.voiceAdapterCreator,
-      selfDeaf: false
+      selfDeaf: false,
     });
     const player = this.players.getByID(message.guildId)?.player ?? createAudioPlayer();
     if (!this.players.has(message.guildId)) {
       connection.on(VoiceConnectionStatus.Disconnected, async () => {
         try {
-          await entersState(connection, VoiceConnectionStatus.Connecting, 5000)
+          await entersState(connection, VoiceConnectionStatus.Connecting, 5000);
         } catch {
           connection.destroy();
           player.stop();
@@ -67,24 +77,27 @@ export class AudioService extends SystemService {
         }
       });
     }
-    this.players.store({
-      guildId: message.guildId,
-      player: player,
-      connection: connection
-    }, true);
+    this.players.store(
+      {
+        guildId: message.guildId,
+        player: player,
+        connection: connection,
+      },
+      true,
+    );
   }
   play(message: Message) {
     const player: AudioPlayer = this.players.getByID(message.guildId).player;
     let connection = getVoiceConnection(message.guildId);
-    let songName = message.content.match(/^!play (.+)/)?.[1] // matches !play (some song name)
+    let songName = message.content.match(/^!play (.+)/)?.[1]; // matches !play (some song name)
     songName && this.startSong(songName, player);
     player.unpause();
     connection.subscribe(player);
   }
   async startSong(songName: string, player: AudioPlayer) {
     const videoResult = (await YTSR(songName + "official audio", { limit: 1 })).items[0];
-    const song = await AudioDownloader.download(videoResult.url)
-    const audio = createAudioResource(song, {inlineVolume: false, inputType: StreamType.Opus});
+    const song = await AudioDownloader.download(videoResult.url);
+    const audio = createAudioResource(song, { inlineVolume: false, inputType: StreamType.Opus });
     // audio.volume.setVolume(0.5)
     player.play(audio);
   }
